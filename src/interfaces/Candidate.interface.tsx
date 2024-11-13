2b09d72cb0fb6c49ceddae33f3da84117a7ff376
riley <mechamriley@gmail.com>
2024-11-13 15:43:00 -0700

    added id to the candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,5 +1,6 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 interface Candidate {
+    id: number;
     login: string;
     location: string;
     avatar_url: string;
